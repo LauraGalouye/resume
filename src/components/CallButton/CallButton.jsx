de53d0f7b81b577a2334c19bea52b54e94be1f5f
Laura Galouye <lgalo@MacBook-Air.lan>
2024-04-03 19:52:48 +0200

    contact correction

--- src/components/CallButton/CallButton.jsx
+++ src/components/CallButton/CallButton.jsx
@@ -4,9 +4,9 @@ import "../CallButton/CallButton.scss";
 const CallButton = ({ phoneNumber }) => {
     return (
         <a  href={`tel:${phoneNumber}`} className="call-button">
-            <button>
+            
                 06.89.74.20.63
-            </button>
+            
         </a>
     );
 }
--- src/components/EmailLink/EmailLink.jsx
+++ src/components/EmailLink/EmailLink.jsx
@@ -3,9 +3,9 @@ import "./EmailLink.scss";
 const EmailLink = ({ email }) => {
     return (
         <a href={`mailto:${email}`} className="email-button">
-            <button >
+            
                 lauragalouye@yahoo.fr
-            </button>
+            
         </a>
     );
 }
